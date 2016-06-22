4505b704f74a22bb8d44e93b36df093c1ee3cdbb
santo <santoshvijaypawar@gmail.com>
2016-06-22 10:12:20 +0530

    Update routes.php
    This frame will contain a login screen, where a user will be able to register / login.
    After a user logs in, he will be able to create/edit/delete a post, which will appear on the websites homepage.
    
    Additional Gallery Simple Application

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,10 +1,8 @@
 <?php
-Route::get('gallery/list','GalleryController@viewGalleryList');
-Route::post('gallery/save','GalleryController@saveGallery');
-Route::get('gallery/view/{id}','GalleryController@viewGalleryPics');
-Route::post('image/do-upload','GalleryController@doImageUpload');
-
-
+Route::get('gallery/list', 'GalleryController@viewGalleryList');
+Route::post('gallery/save', 'GalleryController@saveGallery');
+Route::get('gallery/view/{id}', 'GalleryController@viewGalleryPics');
+Route::post('image/do-upload', 'GalleryController@doImageUpload');
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +13,7 @@ Route::post('image/do-upload','GalleryController@doImageUpload');
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 
 //Route::get('student','StudentController@index');
 //Route::get('student/create','StudentController@create');
@@ -24,55 +22,43 @@ Route::post('image/do-upload','GalleryController@doImageUpload');
 /*
  * Blog CRUD
  */
-Route::resource('blog','blogController');
+Route::resource('blog', 'blogController');
 //Route::get('blog/create',['middleware'=>'auth','uses'=>'blogController@create']);
 //Route::get('blog',['middleware'=>'auth','uses'=>'blogController@index']);
 //Route::get('bog','blogController')
 //Route::get('blog','blogController@index');
 //Route::get('blog/create','blogController@create');
 /*Route::resource('blog/create',[
-    'middleware'=>'auth',
-    'uses'=>'blogController'
+'middleware'=>'auth',
+'uses'=>'blogController'
 ]);*/
 
 /*
  * Register and login authentication
-*/
+ */
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
-
 
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 Route::post('auth/test', 'Auth\AuthController@postTest');
-get( '/logout' , [
-    'as' => 'logout' ,
-    'uses' => 'Auth\AuthController@getLogout'
-] );
+get('/logout', [
+    'as' => 'logout',
+    'uses' => 'Auth\AuthController@getLogout',
+]);
 
 /*
  * Students and task
-*/
+ */
 
-
-
-
-
-
-
-Route::resource('student','studentController');
-Route::resource('pages','PagesController');
-Route::resource('tasks','TasksController');
+Route::resource('student', 'studentController');
+Route::resource('pages', 'PagesController');
+Route::resource('tasks', 'TasksController');
 
 Route::get('/', [
     'as' => 'home',
-    'uses' => 'PagesController@home'
+    'uses' => 'PagesController@home',
 ]);
 
 //Route::resource("student", "studentController");
-
-
-
-
-

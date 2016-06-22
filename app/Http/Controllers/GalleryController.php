977d151b951d97699cab4ee5d52c30bdaf3b5a35
santo <santoshvijaypawar@gmail.com>
2016-06-22 10:13:57 +0530

    Gallery Controller add ,upload images

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Auth;
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
 use App\Gallery;
-use App\Image;
+use App\Http\Controllers\Controller;
+use Auth;
+use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
@@ -16,12 +14,14 @@ class GalleryController extends Controller
      *
      * @return Response
      */
-    public function __construct(){
+    public function __construct()
+    {
         $this->middleware('auth');
     }
+
     public function viewGalleryList()
     {
-        $gallery=Gallery::latest()->get();
+        $gallery = Gallery::latest()->get();
         return view('gallery.gallery', compact('gallery'));
         //
     }
@@ -43,16 +43,15 @@ class GalleryController extends Controller
      */
     public function saveGallery(Request $request)
     {
-        $this->validate($request,[
-            'name'=>'required|min:3'
+        $this->validate($request, [
+            'name' => 'required|min:3',
 
         ]);
         //Auth::user();
 
-        $request['created_by']= Auth::user()->id;
-        $request['published']= Auth::user()->id;
-        $galleryData=Gallery::create($request->all());
-
+        $request['created_by'] = Auth::user()->id;
+        $request['published'] = Auth::user()->id;
+        $galleryData = Gallery::create($request->all());
 
         return redirect()->back();
         //
@@ -66,8 +65,8 @@ class GalleryController extends Controller
      */
     public function viewGalleryPics($id)
     {
-        $gallery=Gallery::findOrfail($id);
-       return view('gallery.gallery-view',compact('gallery'));
+        $gallery = Gallery::findOrfail($id);
+        return view('gallery.gallery-view', compact('gallery'));
         //
     }
 
@@ -102,23 +101,23 @@ class GalleryController extends Controller
     public function doImageUpload(Request $request)
     {
         //get the file from post request
-          $file=$request->file('file');
+        $file = $request->file('file');
 
         //set my filename
-          $filename=uniqid().$file->getClientOriginalName();
+        $filename = uniqid() . $file->getClientOriginalName();
 
         //move file to corect location
-           $file->move('gallery/images/',$filename);
-            //save in db
-        $gallery=Gallery::find($request->input('gallery_id'));
+        $file->move('gallery/images/', $filename);
+        //save in db
+        $gallery = Gallery::find($request->input('gallery_id'));
 
-        $image=$gallery->images()->create([
-            'gallery_id'=>$request->input('gallery_id'),
-            'file_name'=>$filename,
-            'file_size'=>$file->getClientSize(),
-            'file_mime'=>$file->getClientMimeType(),
-            'file_path'=>'Gallery/images/'.$filename,
-            'created_by'=>Auth::user()->id,
+        $image = $gallery->images()->create([
+            'gallery_id' => $request->input('gallery_id'),
+            'file_name' => $filename,
+            'file_size' => $file->getClientSize(),
+            'file_mime' => $file->getClientMimeType(),
+            'file_path' => 'Gallery/images/' . $filename,
+            'created_by' => Auth::user()->id,
 
         ]);
         return $image;
